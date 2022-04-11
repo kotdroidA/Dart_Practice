7b044fd2a24119a8df913bdd512e851d32bf5ca7
Anchal <anchal.sri1688@gmail.com>
2022-04-11 17:22:02 +0530

    anonymous_function

---
+++ anonymous_function.dart
@@ -0,0 +1,10 @@
+var a = (String name) {
+  print("I am ${name}");
+};
+void main() {
+  a("nounio");
+  var b = [1, 4, 63, 2];
+  b.forEach((element) {
+    print(" bana rhe hai ${element}");
+  });
+}
