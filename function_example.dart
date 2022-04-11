5f84e81912a57d142f15738c2f718ba99d2d53c5
Anchal <anchal.sri1688@gmail.com>
2022-04-11 16:57:16 +0530

    functions paramerter type and lambda function

--- function_example.dart
+++ function_example.dart
@@ -1,8 +1,31 @@
+///function with 2 positional parameter
 double calculatePercent(int maxiMumMarks, int marksObtain) {
   double percentage = (marksObtain * 100) / maxiMumMarks;
   return percentage;
 }
 
+///function with two positional parameter and 1 optional positional parameter
+void displayName(String firstName, int age,
+    [String? surName, String? fatherName]) {
+  print(
+      "my name is ${firstName} ${surName}.I am ${age} year old.my father name is ${fatherName}");
+}
+
+///function with 2 positional parameter and 1 named parameter and 1 named parameter with default value
+void displayEmpolyeeInfo(name, department, {experience, officeName: "google"}) {
+  print(
+      "my name is ${name}.I work in ${officeName} with ${department} team.I have ${experience} years of experience.");
+}
+
 main() {
   print(calculatePercent(600, 455).toStringAsFixed(2));
+
+  displayName("Anchal", 22);
+  displayName("bandar", 3, "madari");
+  displayName("aaru", 12, "", "Ramesh");
+
+  displayEmpolyeeInfo("pushkar", "flutter");
+  displayEmpolyeeInfo("mohit", "android", experience: 5);
+  displayEmpolyeeInfo("Rashmi", "testing",
+      officeName: "micosoft", experience: 5);
 }
---
+++ lambda_function.dart
@@ -0,0 +1,4 @@
+int addNum(int num1, int num2) => num1 + num2;
+main() {
+  print("add two numbers ${addNum(5, 6)}");
+}
