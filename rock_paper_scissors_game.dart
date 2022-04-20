900b033f5e124a2082654dab685128aa0f04bfbe
Anchal <anchal.sri1688@gmail.com>
2022-04-20 15:52:30 +0530

    Rock-paper-Scissors

--- rock_paper_scissors_game.dart
+++ rock_paper_scissors_game.dart
@@ -5,12 +5,30 @@ import 'dart:math';
 void main(List<String> args) {
   //Make a two-player Rock-Paper-Scissors game against computer.
 
-  var ran = Random();
-  stdout.write(
-      "start game please enter your move rock=0, paper=1, scissors=2 \n please enter q or quit your game.");
+  Random random = new Random();
+  int randaomNumber = random.nextInt(2);
 
-  var userMove = int.parse(stdin.readLineSync()!);
+  stdout.write(
+      "start game please enter your move rock=0, paper=1, scissors=2,c your move q or quit your game.");
+
+  var userMove = stdin.readLineSync();
   while (userMove != "q") {
-    userMove = int.parse(stdin.readLineSync()!);
+    if (userMove != "0" && userMove != "1" && userMove != "2") {
+      print("Soooorrrry! your move is Invalid. please try again.");
+    } else {
+      var userMove1 = int.parse(userMove!);
+      if (userMove == randaomNumber) {
+        print("game draw!");
+      } else if (userMove1 == 0 && randaomNumber == 2 ||
+          userMove1 == 1 && randaomNumber == 0 ||
+          userMove1 == 2 && randaomNumber == 1) {
+        print("you win!");
+      } else {
+        print("you lost!");
+      }
+    }
+    randaomNumber = random.nextInt(2);
+    print("Please enter your move to play.. or q to quit.");
+    userMove = stdin.readLineSync();
   }
 }
