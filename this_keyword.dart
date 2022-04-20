cd38aed876a46521e48e3d76bd5093f767801b45
Anchal <anchal.sri1688@gmail.com>
2022-04-20 17:18:12 +0530

    this keywod example

---
+++ this_keyword.dart
@@ -0,0 +1,14 @@
+class Student {
+  var name;
+  Student(name) {
+    this.name;
+  }
+  printName() {
+    print("I am $name.");
+  }
+}
+
+void main(List<String> args) {
+  Student s1 = Student("Ram");
+  s1.printName();
+}
